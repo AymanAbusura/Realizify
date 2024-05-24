bebe8bf4148f4aa6bead79730aa4c7e215054ea7
Ayman Salameh <ayman@MacBook-Air-Ayman.local>
2024-05-24 10:15:51 +0300

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// const TransformationsPage = ({ params: { id } }) => {
+const TransformationsPage = () => {
+  return (
+    <div>TransformationsPage</div>
+  )
+}
+
+export default TransformationsPage
